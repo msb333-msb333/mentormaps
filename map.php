45e14ae1030ec55e31f315c87c0fc3827a5fd812
Joseph Sirna <joseph.sirna@gmail.com>
2015-08-07 16:57:50 -0700

    remove useless formula

--- map.php
+++ map.php
@@ -61,22 +61,6 @@ while($r=mysqli_fetch_assoc($result)){
 		<!--<script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?libraries=geometry&sensor=false"></script>-->
     <script type="text/javascript">
 	
-	var rad = function(x) {
-  return x * Math.PI / 180;
-};
-
-var getDistance = function(p1, p2) {
-  var R = 6378137; // Earth’s mean radius in meter
-  var dLat = rad(p2.lat() - p1.lat());
-  var dLong = rad(p2.lng() - p1.lng());
-  var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-    Math.cos(rad(p1.lat())) * Math.cos(rad(p2.lat())) *
-    Math.sin(dLong / 2) * Math.sin(dLong / 2);
-  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-  var d = R * c;
-  return d; // returns the distance in meter
-};
-	
       function initialize() {
         var map = new google.maps.Map(document.getElementById('map-canvas'),{zoom: 11});
 		geocoder = new google.maps.Geocoder();
