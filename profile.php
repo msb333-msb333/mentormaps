78d02588708339fbae2f1dfeabc269d814a76a99
itsjustjon <jonathan.logrippo@servitehs.org>
2015-08-12 16:20:43 -0700

    Work on Profile

--- profile.php
+++ profile.php
@@ -1,6 +1,7 @@
 <?php
 //dump all profile data for the provided email
     if(isset($_GET['p'])){
+        require "./core.php";
         $refurl = "./profile.php?p=" . $_GET['p'];
         require "./logincheck.php";
         require "./db.php";
@@ -28,17 +29,73 @@
             $age = $i['AGE'];
             $account_type = $i['ACCOUNT_TYPE'];
         }
-        
-        echo "Name: $name<br />";
-        echo "Skills JSON: $skills_json<br />";
-        echo "Team Number: $team_number<br />";
-        echo "Comments: $comments<br />";
-        echo "Phone: $phone<br />";
-        echo "Email: $email<br />";
-        echo "Address: $address<br />";
-        echo "Type: $type<br />";
-        echo "Age: $age<br />";
-        echo "Account Type: $account_type<br />";
+        echoHeader();
+?>
+    <article id="main"> 
+        <section class="wrapper style5">
+            <header>
+                <h3><?php echo $name . "'s"?> Profile Page</h3>
+            </header>
+                <div id="personal-info"style="padding-left: 25px;">
+                    <h4>Personal Info</h4>
+                </div>
+                    <div id="name-div" style="padding-left: 60px;">
+                        <b>Name:</b>   
+                            <?php 
+                                echo $name;
+                            ?>
+                    </div>
+                    <div id="age-div" style="padding-left: 60px;">
+                        <b>Age:</b>   
+                            <?php 
+                                echo $age;
+                            ?>
+                    </div>
+                    <div id="email-div" style="padding-left: 60px;">
+                        <b>Email Address:</b>   
+                            <?php 
+                                echo $email;
+                            ?>
+                    </div>
+                    <div id="address-div" style="padding-left: 60px;">
+                        <b>Address:</b>   
+                            <?php 
+                                echo $address;
+                            ?>
+                    </div>
+                    <div id="phone-div" style="padding-left: 60px;">
+                        <b>Phone Number:</b>   
+                            <?php 
+                                echo $phone;
+                            ?>
+                    </div>
+                    <div id="type-div" style="padding-left: 60px;">
+                        <b>Program Affiliation:</b>   
+                            <?php 
+                                echo $type;
+                            ?>
+                    </div>
+                <div class="inner">
+                    <?php /*
+                    
+                        $array = json_decode($skills_json, true);
+                        
+                        $assoc_array = array();
+                        for($i = 0; $i < sizeof($array); $i++){
+                           $key = $array[$i]['name'];
+                           $assoc_array[$key] = $array[$i]['value'];
+                        }
+                        foreach($assoc_array as $element) {
+                            if($assoc_array[$element] == 'true'){
+                                echo $element;
+                            }
+                        }
+                        
+                    */?>    
+                </div>
+        </section>
+    </article>
+<?php
         
     }else{
         die('please specify a user');
