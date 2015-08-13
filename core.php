98dd089dc8421813aa4b5ecfaf1b7010b5a96334
Joseph Sirna <joseph.sirna@gmail.com>
2015-08-13 08:40:49 -0700

    consolidate profile link code

--- core.php
+++ core.php
@@ -2,4 +2,13 @@
 function echoHeader(){
     include "./pages/header.html";
 }
+
+function echoProfileLink(){
+    require "./sessioncheck.php";
+    if($_SESSION['auth']==false || !isset($_SESSION['auth']) || !isset($_SESSION['email'])){
+        echo './login.php';
+    }else{
+        echo './profile.php?p=' . $_SESSION['email'];
+    }
+}
 ?>
