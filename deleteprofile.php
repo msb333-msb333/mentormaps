2f9f915724c4bf844f3c449e9af8b037113f9949
Joseph Sirna <joseph.sirna@gmail.com>
2015-08-13 08:31:11 -0700

    added debugging to deleteprofile.php

--- deleteprofile.php
+++ deleteprofile.php
@@ -3,6 +3,10 @@ require "./logincheck.php";
 checkIfUserLoggedIn($_SESSION['email']);
 require "./db.php":
 
-$db->query(mysql_escape_mimic("DELETE FROM `logins` WHERE EMAIL = '" . $_POST['user_to_delete'] . "' LIMIT 1;"));
-$db->query(mysql_escape_mimic("DELETE FROM `data` WHERE EMAIL = '".$_POST['user_to_delete']."' LIMIT 1;"));
+$sql = mysql_escape_mimic("DELETE FROM `logins` WHERE EMAIL = '" . $_POST['user_to_delete'] . "' LIMIT 1;");
+$db->query($sql);
+file_put_contents("./result.txt", $sql);
+$sql = mysql_escape_mimic("DELETE FROM `data` WHERE EMAIL = '".$_POST['user_to_delete']."' LIMIT 1;");
+$db->query($sql);
+file_put_contents("./result2.txt", $sql);
 ?>
