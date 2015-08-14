245d5a2957abea2f18a8e10c25833b548b973af7
Joseph Sirna <joseph.sirna@gmail.com>
2015-08-14 16:06:21 -0700

    remove debug & useless files

--- storeaddress.php
+++ storeaddress.php
@@ -1,12 +1,11 @@
 <?php
 if($_SERVER['REQUEST_METHOD'] == 'POST'){
     require "./db.php";
-    $address = $_POST['address'];
+    $address = mysql_escape_mimic($_POST['address']);
     $longitude = $_POST['longitude'];
     $latitude = $_POST['latitude'];
     $sql = "INSERT INTO `locations` (ADDRESS, LATITUDE, LONGITUDE) VALUES ('$address', '$latitude', '$longitude')";
     $db->query($sql);
-    file_put_contents("./query.txt", $sql);
     echo '{"status":"queried successfully"}';
 }else{
     die("post");
