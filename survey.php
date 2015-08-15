8b4dff469e2e68e2e3fa2ffb432c8c8d68b03ef1
Joseph Sirna <joseph.sirna@gmail.com>
2015-08-14 21:16:18 -0700

    stuff

--- survey.php
+++ survey.php
@@ -1,27 +1,38 @@
-<link rel="stylesheet" href="css/foundation.css">
-<link rel="stylesheet" href="css/app.css">
 <?php
     require "./core.php";
     echoHeader();
 ?>
-  <script src="js/foundation/foundation.js"></script>
-  <script src="js/foundation/foundation.slider.js"></script>
-  <script src="js/vendor/modernizr.js"></script>
 
-<article id="main">
+<script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+<link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
+
+<script>
+  $(function() {
+    $( "#slider" ).slider({
+        min: 1,
+        max: 10,
+        change: function(event, ui){
+            $("#slider-display").html(ui.value);
+        }
+    });
+  });
+</script>
+
+<article id="main" style="width:100%;">
     <header>
         <h2>Mentor Maps Survey</h2>
         <h3 style="color:#c1c1c1">Your answers will be collected to improve MentorMaps</h3> 
     </header>
-    <section class="wrapper style5">
-        <div align='center' style="padding-top:10px; padding-bottom:10px">
+    <section class="wrapper style5" style="width:100%;">
+        <div style="display:block;width:100%;text-align:center;margin:auto;padding-top:10px; padding-bottom:10px">
             <h3>Rate your experience</h3>
-            <div id="slider1" class="range-slider" data-slider data-options="step: 1;">
-              <span class="range-slider-handle"></span>
-              <span class="range-slider-active-segment"></span>
-              <input type="hidden">
+            <div id="slider-wrapper" style="width:50%;margin:auto;">
+                <div id="slider"></div><div id="slider-display"></div>
             </div>
-            <hr>
+                <script>
+                    $("#slider-display").html(ui.value);
+                </script>
+            <hr />
             <h3>Would you recommend MentorMaps to a friend?</h3>
             <div class="row uniform" style="display: inline-block;">
                 <div class="3u 12u$">
@@ -32,26 +43,29 @@
                     <label for="no">No</label>
                 </div>
             </div>
-            <hr>
+            <hr />
             <h3 style="padding-top:10px; padding-bottom:10px">What features did you like or use the most?</h3>
             <div align='center' style="padding-top:10px; padding-bottom:10px">
                 <input type="text" name="team-name" id="team-name" placeholder="Write Response" style="width: 60%"/>
             </div>
-            <hr>
+            <hr />
             <h3 style="padding-top:10px; padding-bottom:10px">What features did you dislike or never use?</h3>
             <div align='center' style="padding-top:10px; padding-bottom:10px">
                 <input type="text" name="team-name" id="team-name" placeholder="Write Response" style="width: 60%"/>
             </div>
-            <hr>
+            <hr />
             <h3 style="padding-top:10px; padding-bottom:10px">What features would you like to see added?</h3>
             <div align='center' style="padding-top:10px; padding-bottom:10px">
                 <input type="text" name="team-name" id="team-name" placeholder="Write Response" style="width: 60%"/>
             </div>
+            </div>
+        </section>
+    </article>
+            <footer id="footer">
+                <ul class="copyright">
+                    <li>&copy; Joseph Sirna 2015</li>
+                </ul>
+            </footer>
         </div>
-    </section>
-    <script>
-        $(document).foundation();
-    </script>
-    
-</article>
-
+    </body>
+</html>
