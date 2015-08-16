e2f7c6cffcf0b87e5e035f6669b1ab04a0d08912
Joseph Sirna <joseph.sirna@gmail.com>
2015-08-15 17:28:30 -0700

    readd function

--- map.php
+++ map.php
@@ -313,6 +313,22 @@ while($r=mysqli_fetch_assoc($result)){
                         echo 'var allteams = ' . json_encode($allteams) . ";" . PHP_EOL;
                     ?>
                     
++   var rad = function(x) {
++  return x * Math.PI / 180;
++};
++
++var getDistance = function(p1, p2) {
++  var R = 6378137; // Earth’s mean radius in meter
++  var dLat = rad(p2.lat() - p1.lat());
++  var dLong = rad(p2.lng() - p1.lng());
++  var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
++    Math.cos(rad(p1.lat())) * Math.cos(rad(p2.lat())) *
++    Math.sin(dLong / 2) * Math.sin(dLong / 2);
++  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
++  var d = R * c;
++  return d; // returns the distance in meter
++};
+                    
                     var me;
                     for(var i=0;i<alldata.length;i++){
                         var current_item = alldata[i];
