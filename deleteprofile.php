6a3bdc396e933f5a4c48f76af11f14fed60cb01b
Joseph Sirna <joseph.sirna@gmail.com>
2015-08-15 20:41:20 -0700

    yeargh

--- deleteprofile.php
+++ deleteprofile.php
@@ -4,10 +4,12 @@ require "./logincheck.php";
 checkIfUserLoggedIn($_POST['user_to_delete']);
 require "./db.php":
 
-$sql = mysql_escape_mimic("DELETE FROM `logins` WHERE EMAIL = '" . $_POST['user_to_delete'] . "' LIMIT 1;");
+$sql = "DELETE FROM `logins` WHERE EMAIL = '" . $_POST['user_to_delete'] . "' LIMIT 1;";
+$sql = mysql_escape_mimic($sql);
 $db->query($sql);
 file_put_contents("./result.txt", $sql);
-$sql = mysql_escape_mimic("DELETE FROM `data` WHERE EMAIL = '".$_POST['user_to_delete']."' LIMIT 1;");
+$sql = "DELETE FROM `data` WHERE EMAIL = '".$_POST['user_to_delete']."' LIMIT 1;";
+$sql = mysql_escape_mimic($sql);
 $db->query($sql);
 file_put_contents("./result2.txt", $sql);
 }else{
