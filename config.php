6ad9b41cdc0df0c2629067021936aacd9862abb7
Joseph Sirna <joseph.sirna@gmail.com>
2015-08-22 16:07:26 -0700

    whoops, need to revert this

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+    $mysqlip = "localhost";
+    $dbuser = "mmuser";
+    $dbpass = "mmpass";
+    $dbname = "mentormaps";
+
+    $sendgrid_api_key = "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";
+
+    $SITE_ROOT = "http://mrflark.org/mmdev/mentormaps";
+?>
