6a1c460987402132654aa2ee219e9125ff222689
Joseph Sirna <joseph.sirna@gmail.com>
2015-08-21 21:58:00 -0700

    readd lib through composer & working mail sender

--- mailtest.php
+++ mailtest.php
@@ -1,4 +1,14 @@
 <?php
-    require("./assets/php/sendgrid-php-3.2.0/lib/SendGrid.php");
-    
+    require "./config.php";
+    require("./vendor/autoload.php");
+
+    function sendEmail($to, $subject, $html){
+        $sendgrid = new SendGrid($sendgrid_api_key);
+        $email    = new SendGrid\Email();
+        $email->addTo($to)
+            ->setFrom("donotreply@mentormaps.net")
+            ->setSubject($subject)
+            ->setHtml($html);
+        $sendgrid->send($email);
+    }
 ?>
