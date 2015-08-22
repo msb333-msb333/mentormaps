c59fd33e1e6f14a0db7f0938e8df69638be6dc78
Joseph Sirna <joseph.sirna@gmail.com>
2015-08-22 10:18:01 -0700

    redirect

--- verify.php
+++ verify.php
@@ -3,6 +3,5 @@
     $key = $_GET['key'];
     $sql = "UPDATE `logins` SET `VERIFIED` = 'true' WHERE `KEY` = '$key'";
     $db->query($sql);
-
-    
+    echo '<script>window.location = "./login.php";</script>';
 ?>
