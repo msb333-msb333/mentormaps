ddef5e2582d8a2a39a238417f9b8fa0df7cc1622
Joseph Sirna <joseph.sirna@gmail.com>
2015-08-22 21:32:59 -0700

    finalize email setup

--- emailtest.php
+++ emailtest.php
@@ -6,5 +6,5 @@ $part1 = file_get_contents('./pages/email_header.html');
 $part2 = file_get_contents('./pages/email_footer.html');
 $var = "<a href='http://mrflark.org/mmdev/mentormaps/index.php'>test</a>";
 
-sendEmail($part1 . $var . $part2);
+sendEmail($sendgrid_api_key, 'jonathan.logrippo@servitehs.org', 'SUBJECT', $part1 . $var . $part2);
 ?>
