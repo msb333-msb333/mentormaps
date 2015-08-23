089e7af2da57996e753a9a5dd740206b0f19654a
Joseph Sirna <joseph.sirna@gmail.com>
2015-08-22 21:56:51 -0700

    stuff

--- emailtest.php
+++ emailtest.php
@@ -2,8 +2,8 @@
 require "./config.php";
 require "./mailsender.php";
 
-$part1 = file_get_contents('./pages/email_header.html');
-$part2 = file_get_contents('./pages/email_footer.html');
+$part1 = file_get_contents('./pages/reset_pass_header.html');
+$part2 = file_get_contents('./pages/reset_pass_footer.html');
 $var = "<a href='http://mrflark.org/mmdev/mentormaps/index.php'>test</a>";
 
 sendEmail($sendgrid_api_key, 'jonathan.logrippo@servitehs.org', 'SUBJECT', $part1 . $var . $part2);
