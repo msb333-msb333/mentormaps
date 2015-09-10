0876986489a052a6c08b4db621a755046013e952
Joseph Sirna <joseph.sirna@gmail.com>
2015-09-10 15:29:36 -0700

    remove some unneeded files

--- sessioncheck.php
+++
@@ -1,6 +0,0 @@
-<?php
-//starts the php session if it isn't already
-if (session_status() == PHP_SESSION_NONE) {
-    session_start();
-}
-?>
--- verify.php
+++
@@ -1,7 +0,0 @@
-<?php
-    require "./db.php";
-    $key = $_GET['key'];
-    $sql = "UPDATE `logins` SET `VERIFIED` = 'true' WHERE `KEY` = '$key'";
-    $db->query($sql);
-    echo '<script>window.location = "./login.php";</script>';
-?>
