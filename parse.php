3c2be3f996fe821c4aa377486f3b395eadc2f0b6
Joseph Sirna <joseph.sirna@gmail.com>
2015-09-10 21:37:05 -0700

    add parse library

---
+++ parse.php
@@ -0,0 +1,7 @@
+<?php
+    require 'vendor/autoload.php';
+ 
+    use Parse\ParseClient;
+ 
+    ParseClient::initialize('883aq7xdHmsFK7htfN2muJ5K3GE6eXWDiW7WwdYh', 'D7txJ46SLuykYAGM0tonk5yqyfAcczeAgttRtpn5', 'UeYP2iJDeWpLZyVEnF2fjxG2aRPpCw4eiDkH2h3M');
+?>
