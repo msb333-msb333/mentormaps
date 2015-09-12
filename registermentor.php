ae875e8e377058948a9bf182bdcb46d3bb1608f7
Joseph Sirna <joseph.sirna@gmail.com>
2015-09-12 15:29:13 -0700

    formatting

--- registermentor.php
+++ registermentor.php
@@ -94,25 +94,21 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
         <title>Mentor Maps</title>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
         <link rel="stylesheet" href="assets/css/main.css" />
-        <!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
-        <!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
         <script src="assets/js/jquery.min.js"></script>
         <script src="assets/js/jquery.scrollex.min.js"></script>
         <script src="assets/js/jquery.scrolly.min.js"></script>
         <script src="assets/js/skel.min.js"></script>
         <script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?key=AIzaSyC-e-RpEFPKNX-hDqBs--zoYYCk2vmXdZg"></script>
         <script src="assets/js/util.js"></script>
-        <!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
         <script src="assets/js/main.js"></script>
+        <!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+        <!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+        <!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+        <!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
     </head>
     <body class="landing">
-
-        <!-- Page Wrapper -->
             <div id="page-wrapper">
-
-                <!-- Header -->
                     <header id="header">
                         <h1><a href="./index.php">Mentor Maps</a></h1>
                         <nav id="nav">
@@ -193,9 +189,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
                                                 <br />&nbsp;
                                             </div>
 
-                                            <?php
-                                                include("./pages/skillset_form.html");
-                                            ?>
+                                            <?php include("./pages/skillset_form.html"); ?>
 
                                             <div class="12u$">
                                                 <textarea name="bio" maxlength="200" title="Write something about yourself" id="bio" placeholder="Write something about yourself" rows="6"></textarea>
@@ -213,19 +207,15 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
                             </div>
                         </section>
                     </article>
-
-                <!-- Footer -->
                     <footer id="footer">
                         <ul class="copyright">
                             <li>&copy; FRC Team 3309, 2015</li>
                         </ul>
                     </footer>
-
             </div>
     </body>
     <script src="./customjquery.js"></script>
     <script>
-        //disable uls by default
         $(function(){
             $("#engineering-types-list").toggle();
             $("#programming-types-list").toggle();
