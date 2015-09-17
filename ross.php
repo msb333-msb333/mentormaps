577fd40ba4b3a25b23cf15ec9facdbf66446ca1f
Joseph Sirna <joseph.sirna@gmail.com>
2015-09-16 18:12:45 -0700

    remove db truncator

--- ross.php
+++
@@ -1,7 +0,0 @@
-<?php
-    require "./db.php";
-    $db->query("TRUNCATE `logins`;");
-    $db->query("TRUNCATE `data`;");
-    $db->query("TRUNCATE `survey_results`;");
-    $db->query("TRUNCATE `assoc`;");
-?>
