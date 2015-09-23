dd83b0705c68cf622c9a2f108d1a8945c6423cc7
Joseph Sirna <joseph.sirna@gmail.com>
2015-09-22 17:47:37 -0700

    stuff

--- map.php
+++ map.php
@@ -232,19 +232,19 @@ echo '<script>var marker_map = [];</script>';
     </title>
     <meta charset="utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1"/>
-    <link rel="stylesheet" href="assets/css/main.css"/>
-    <script src="compare.js"></script>
-    <script src="assets/js/jquery.min.js"></script>
-    <script src="assets/js/jquery.scrollex.min.js"></script>
-    <script src="assets/js/jquery.scrolly.min.js"></script>
-    <script src="assets/js/skel.min.js"></script>
-    <script src="assets/js/util.js"></script>
-    <script src="assets/js/main.js"></script>
+    <link rel="stylesheet" href="https://mentormaps.net/assets/css/main.css"/>
+    <script src="https://mentormaps.net/compare.js"></script>
+    <script src="https://mentormaps.net/assets/js/jquery.min.js"></script>
+    <script src="https://mentormaps.net/assets/js/jquery.scrollex.min.js"></script>
+    <script src="https://mentormaps.net/assets/js/jquery.scrolly.min.js"></script>
+    <script src="https://mentormaps.net/assets/js/skel.min.js"></script>
+    <script src="https://mentormaps.net/assets/js/util.js"></script>
+    <script src="https://mentormaps.net/assets/js/main.js"></script>
     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAiDYjxvrOGR6epXYDkO3XaZeT37OEix_Q"></script>
-    <!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
-    <!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
-    <!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css"/><![endif]-->
-    <!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css"/><![endif]-->
+    <!--[if lte IE 8]><script src="https://mentormaps.net/assets/js/ie/html5shiv.js"></script><![endif]-->
+    <!--[if lte IE 8]><script src="https://mentormaps.net/assets/js/ie/respond.min.js"></script><![endif]-->
+    <!--[if lte IE 8]><link rel="stylesheet" href="https://mentormaps.net/assets/css/ie8.css"/><![endif]-->
+    <!--[if lte IE 9]><link rel="stylesheet" href="https://mentormaps.net/assets/css/ie9.css"/><![endif]-->
     <script>
         <?php
         echo 'var alldata = ' . json_encode(utf8_converter($all_data)) . ';' . PHP_EOL;
