e47521349cf9d0a8e730a99b116543a3d0f393e2
Joseph Sirna <joseph.sirna@gmail.com>
2015-09-22 18:16:32 -0700

    nope nevermind

--- map.php
+++ map.php
@@ -559,7 +559,7 @@ echo '<script>var marker_map = [];</script>';
                         <input type="checkbox" id="vex"/>
                         <label for="vex">VEX</label>
                         <script>
-                            var types = me.type;
+                            var types = $.parseJSON(me.type);
                             for(type in types){
                                 if(types[type] == 'true'){
                                     $("#"+type).prop("checked", true);
