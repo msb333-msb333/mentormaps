a2ea509a912a2a83dd0fef4fb086c668b37e5bcc
Joseph Sirna <joseph.sirna@gmail.com>
2015-09-22 18:26:58 -0700

    more api

--- api.php
+++ api.php
@@ -4,12 +4,30 @@ require "./db.php";
 $result = $db->query("SELECT * FROM `logins`");
 $a = array();
 
+$teams = 0;
+$verified = 0;
+$total = 0;
+
+
 while($r=mysqli_fetch_array($result)){
+    $total++;
+    if($r['TYPE']=='TEAM'){
+        $teams++;
+    }
+    if($r['VERIFIED']=='true'){
+        $verified++;
+    }
+
+
     array_push($a, array(
         $r['EMAIL'],
         $r['VERIFIED'],
         $r['TYPE']
     ));
 }
+
+$mentors = $total - $teams;
+$unverified = $total - $verified;
+echo "total:$total, verified:$verified, teams:$teams, unverified:$unverified, mentors:$mentors";
 
 die(json_encode($a));
